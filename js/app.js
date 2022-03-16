6d0d4bd64af2b0672618e84e0115e250d305f61d
RatonRoy <royraton18@gmail.com>
2022-03-16 19:01:23 +0600

    add more informations

--- js/app.js
+++ js/app.js
@@ -4,15 +4,27 @@ const loadCountries = () => {
 		.then(data => displayCountries(data));
 }
 const displayCountries = (countries) => {
-	// console.log(countries[0]);
+	console.log(countries[0]);
 	const countryHtml = countries.map(country => getCountryHtml(country));
 	const countriesContainer = document.getElementById('countries');
 	countriesContainer.innerHTML = countryHtml.join(' ');
 	
 };
-const getCountryHtml = country => {
-	/* variable destracturing  */
+// variable destructuring in the parameter sections 
+const getCountryHtml = ({name, flags, capital, area}) => {
+	return `
+	<div class="country-wrap">
+		<img src="${flags.png}" alt="" class = ''>
+		<h2 class ='countryName'> ${name.common} </h2>
+		<h3> Capital : ${capital} </h3> 
+		<h3> Area : ${area} </h3> 
+	</div>
+	`
+}
+// variable destracturing
 	// option one 
+/* const getCountryHtml = country => {
+	
 	const { name, flags } = country;
 	return `
 	<div class="country-wrap">
@@ -20,12 +32,15 @@ const getCountryHtml = country => {
 		<h2 class = 'countryName'> ${name.common} </h2> 
 	</div>
 	`
-	// this is the normal process 
-	/* return `
+} */
+// this is the normal process 
+/* const getCountryHtml = country => {
+
+	return `
 	<div class="country-wrap">
 		<img src="${country.flags.png}" alt="" class = ''>
 		<h2 class = 'countryName'> ${country.name.common} </h2> 
 	</div>
-	` */
-	}
+	`
+	} */
 loadCountries();
